6c1ff8723ede4a62c3916fc764ce4fe713fb8b15
thienlp <thienlp@dkteckgroup.com>
2025-09-17 09:25:16 +0700

    check google install

--- dktlibrary/src/main/java/com/admob/max/dktlibrary/AdmobUtils.kt
+++ dktlibrary/src/main/java/com/admob/max/dktlibrary/AdmobUtils.kt
@@ -2613,7 +2613,7 @@ object AdmobUtils {
                     isTestDevice = testAdResponses.contains(testAdResponse)
                 }
                 referrerUrl?.let {
-                    checkGoogleReviewAds(it)
+//                    checkGoogleReviewAds(it)
                 }
             } catch (_: Exception) {
                 isTestDevice = true
